a4cc1c9263865009cbf7c2ab5dd941eabb5e1c04
Austin Irwin <austindirwin@gmail.com>
2022-08-29 14:45:54 -0400

    intro javascript

--- testjs.js
+++ testjs.js
@@ -25,16 +25,60 @@ function hilariousMouse() {
   ourText.style.color = "black";
   ourText.style.fontSize = "1em"; //1em is the standard font size
   ourText.innerHTML = "<strong>And we changed it back</strong>";
-  
+
 }
 
 
 // get and store button#mybutton
 ourButton = document.getElementById("mybutton");
-
 //create event listener for clicks
 ourButton.addEventListener("click", SMASH);
 
+
+myCard = document.getElementById("card");
+
 function SMASH() {
-    ourText.innerHTML = "I clicked the button";
+  ourText.innerHTML = "I clicked the button";
+  ourText.style.fontSize = "2em";
+  myCard.style.display = "block";
+
+}
+
+
+//get the textfield with the id myinput
+ourInput = document.getElementById("myinput"); //new text
+ourHeading = document.getElementById("header"); // to modify
+
+//textfield learns what listener is connected to its events
+ourInput.addEventListener("change", changeText); //if the text changes
+ourInput.addEventListener("blur", changeText); //the text field loses focus
+//'focus' when we are typing into box
+// when we click out of it, it loses 'focus'
+//       This is a blur event
+
+function changeText() {
+  // get input
+  newtext = ourInput.value;
+  ourHeading.innerHTML = newtext;
+}
+
+myCardImage = document.getElementById("cardimage");
+myCard.addEventListener("click", changeImage);
+
+function changeImage() {
+  if (myCard.style.position == "") { // if original posiiton
+    myCard.style.position = "absolute";
+    myCard.style.top = "100px";
+    myCard.style.left = "100px";
+    myCardImage.style.width = "80px"; //
+    myCardImage.src = "Images/2_of_diamonds.png";
+  }
+  else {
+    myCard.style.position = ""; // "" for a property means use 
+    myCard.style.top = ""; //       oringinal value
+    myCard.style.left = "";
+    myCardImage.style.width = "auto"; //full width
+    myCardImage.src = "Images/2_of_clubs.png"
+  }
+
 }
