889d4832a1dc0f221cc94dd58c9d412182db9cb6
Austin Irwin <austindirwin@gmail.com>
2022-08-30 21:47:02 -0400

    Assignment 6 part 2 and 3

--- testscript.js
+++ testscript.js
@@ -16,3 +16,66 @@ function mouseOut() {
     myitem.style.color = "";
     myitem.style.backgroundColor = "";
 }
+
+
+/*************** BUttons ********************** */
+
+// I spent lots of time trying to make buttons work with li
+//  Items in my list on the Extracurriculars page but no 
+// button interactivity worked at all, and it also made any
+// following buttons stop working as well
+/*
+dropbutton = document.getElementById("dropbutton");
+info = document.getElementsByClassName("info");
+test = document.getElementById("test")
+
+dropbutton.addEventListener("click", BLAM);
+
+function BLAM() {
+    test.style.color = "red";
+    if (info.style.display =="") {
+        info.style.display="none";
+    }
+    else {
+        info.style.display ="";
+    }
+}  */
+
+thebutton = document.getElementById("thebutton");
+otheritem =document.getElementById("buttontest");
+
+thebutton.addEventListener("click", onButtonClick)
+
+function onButtonClick() {
+    otheritem.style.color="red";
+}
+
+
+imgbutton = document.getElementById("imgbutton");
+imgitem = document.getElementById("antelope");
+
+imgbutton.addEventListener("click", BAM)
+
+function BAM() {
+    if (imgitem.style.display == "")
+    {
+        imgitem.style.display = "none";
+    }
+    else {
+        imgitem.style.display="";
+    }
+}
+
+
+/****************************INPUT************ */
+
+//otheritem = document.getElementById("otheritem")
+
+
+textentry = document.getElementById("myinput");
+textentry.addEventListener("change", onChange);
+
+function onChange() {
+    newtext = myinput.value;
+    otheritem.innerHTML = newtext;
+}
